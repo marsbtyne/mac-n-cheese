52bed9400244b048c62b8a618b3b38978679ef60
marsbtyne <mars.ballantyne@gmail.com>
2019-06-11 12:29:22 -0400

    Updates

---
+++ precache-manifest.96f17816999b6474d4e208c3b07f66ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9bf7f34453a6d816b0d7366e717e4f",
+    "url": "/mac-n-cheese/index.html"
+  },
+  {
+    "revision": "890db4bec0c70f15b8fe",
+    "url": "/mac-n-cheese/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mac-n-cheese/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "890db4bec0c70f15b8fe",
+    "url": "/mac-n-cheese/static/js/main.83396a12.chunk.js"
+  },
+  {
+    "revision": "74851bab2eb833e78e41",
+    "url": "/mac-n-cheese/static/js/runtime~main.7844973f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mac-n-cheese/static/media/logo.5d5d9eef.svg"
+  }
+]);
